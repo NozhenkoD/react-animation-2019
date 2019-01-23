4ba6a1c9f68516e66f1d6cda994e8f90ee36de07
Dmitry Nozhenko <nozhenkod@gmail.com>
2019-01-23 09:05:14 +0200

    [fix]

---
+++ build/precache-manifest.ce9056d32c2ae1c0adbc4f73615032fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "caabf7dcff4bd605c156",
+    "url": "/react-animation-2019/static/js/runtime~main.caabf7dc.js"
+  },
+  {
+    "revision": "6b1be3b7d04aeb49eafd",
+    "url": "/react-animation-2019/static/js/main.6b1be3b7.chunk.js"
+  },
+  {
+    "revision": "c7f90405d64d990e13c9",
+    "url": "/react-animation-2019/static/js/1.c7f90405.chunk.js"
+  },
+  {
+    "revision": "6b1be3b7d04aeb49eafd",
+    "url": "/react-animation-2019/static/css/main.a28e1669.chunk.css"
+  },
+  {
+    "revision": "c7f90405d64d990e13c9",
+    "url": "/react-animation-2019/static/css/1.2c9d1e7d.chunk.css"
+  },
+  {
+    "revision": "e042d536e036c4103d87e77950dc8419",
+    "url": "/react-animation-2019/index.html"
+  }
+];
